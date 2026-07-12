20790d7a38bf4bbfae40291edceee113aa841c22
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "sentinel_data_connector_office_365_projects" {
-  description = "All sentinel_data_connector_office_365_project resources"
-  value       = azurerm_sentinel_data_connector_office_365_project.sentinel_data_connector_office_365_projects
-}
 output "sentinel_data_connector_office_365_projects_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_office_365_projects"
-  value       = [for k, v in azurerm_sentinel_data_connector_office_365_project.sentinel_data_connector_office_365_projects : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_office_365_projects, keyed the same as var.sentinel_data_connector_office_365_projects"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_365_project.sentinel_data_connector_office_365_projects : k => v.log_analytics_workspace_id }
 }
 output "sentinel_data_connector_office_365_projects_name" {
-  description = "List of name values across all sentinel_data_connector_office_365_projects"
-  value       = [for k, v in azurerm_sentinel_data_connector_office_365_project.sentinel_data_connector_office_365_projects : v.name]
+  description = "Map of name values across all sentinel_data_connector_office_365_projects, keyed the same as var.sentinel_data_connector_office_365_projects"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_365_project.sentinel_data_connector_office_365_projects : k => v.name }
 }
 output "sentinel_data_connector_office_365_projects_tenant_id" {
-  description = "List of tenant_id values across all sentinel_data_connector_office_365_projects"
-  value       = [for k, v in azurerm_sentinel_data_connector_office_365_project.sentinel_data_connector_office_365_projects : v.tenant_id]
+  description = "Map of tenant_id values across all sentinel_data_connector_office_365_projects, keyed the same as var.sentinel_data_connector_office_365_projects"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_365_project.sentinel_data_connector_office_365_projects : k => v.tenant_id }
 }
 
